2422571be063a49505aad7007ca7932b1cccafd6
sonnenteich <sonnenteich@freenet.de>
2020-02-03 16:27:04 +0100

    Refactoring of CheckMethodes.cs

--- FinalFrontierUnitTest/CheckMethodesTests.cs
+++ FinalFrontierUnitTest/CheckMethodesTests.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using FinalFrontier;
+﻿using FinalFrontier;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Office.Interop.Outlook;
 
 namespace FinalFrontierUnitTest
 {
@@ -12,16 +12,16 @@ namespace FinalFrontierUnitTest
         [TestInitialize]
         public void init()
         {
-
             checkMethods = new CheckMethods();
         }
 
         [TestMethod]
-        public void CheckSender_Tests()
+        public void CheckFreemailer_withFreemailer()
         {
-            //var result = checkMethods.CheckSender("test@tester.com", "senderMail@tester.net", "senderEnvelope@tester.org");
-            //Assert.IsNotNull(result);
-            
+            var inst = "from [1.2.3.4] (helo=smtp3.gmx.de)  by mdbox59.gmx.de with esmtpa(ID exim)(Exim 4.92 #3)	id 1ix9To - 0003ij - BM    for horst.tester@01019gmx.de; Thu, 30 Jan 2020 14:03:32 + 0100";
+            var result = checkMethods.CheckFreeMailers("FreemailerCheckTest", inst, "info@freenet.de");
+
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
@@ -47,7 +47,53 @@ namespace FinalFrontierUnitTest
         {
             var result = checkMethods.CheckKeywords("KeywordTest", "TestRechnungTest.dox");
 
-            Assert.IsNotNull(result);
+            result.AddRange(checkMethods.CheckKeywords("KeywordTest", "http://microsoft.com/TestRechnungTest"));
+
+            Assert.IsTrue(result.Count == 3);
+        }
+
+        [TestMethod]
+        public void CheckDoubleExtensions_withDoubelExtension()
+        {
+            var result = checkMethods.CheckDoubleExtensions("CheckDoubleExtensionsTest", "TestRechnungTest.doc.exe");
+
+             Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void CheckBadExtensions_withBadExtension()
+        {
+            var result = checkMethods.CheckBadExtensions("BadExtensioTest", "test.vbs");
+
+            Assert.IsTrue(result[0].id == "BadExtensioTest");
+        }
+
+        [TestMethod]
+        [DeploymentItem("app.config")]
+        public void CheckBadHashes_withBadHash()
+        {
+            //MailItem mail = new MailItem();
+            //var tmpAttachment = mail.Attachments.Add("app.config");
+
+            //var result = checkMethods.CheckBadHashes("BadHashesTest", tmpAttachment);
+
+            //Assert.IsTrue(result.Count == 0);
+        }
+
+        [TestMethod]
+        public void GetReceiveFromMail_Test()
+        {
+            var result = checkMethods.GetReceiveFromString("FROM [hierIst@derFromText.de] hier kommt noch Text");
+
+            Assert.AreEqual("[hierIst@derFromText.de]", result);
+        }
+
+        [TestMethod]
+        public void GetDomainFromMail_Test()
+        {
+            var result = checkMethods.GetDomainFromMail("notTheDomain@theDomain.net");
+
+            Assert.AreEqual("thedomain.net", result);
         }
     }
 }
